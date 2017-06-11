18a7d043e0f6d7d833d487397ed7e6b9e8d07840
Louise Camlin <lacamlin@gmail.com>
2017-06-11 13:41:30 +0100

    first commit - set up Item, Shopping Basket and ShoppingBasket Test classes

---
+++ shopping_basket/ShoppingBasket.java
@@ -0,0 +1,15 @@
+package shopping_basket;
+
+import java.util.*;
+import java.util.ArrayList;
+
+public class ShoppingBasket {
+
+
+  private ArrayList<Item> basket;
+
+  public ShoppingBasket() {
+    this.basket = new ArrayList<Item>();
+  }
+
+}
---
+++ specs/ShoppingBasketTest.java
@@ -0,0 +1,15 @@
+import shopping_basket.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ShoppingBasketTest {
+
+  ShoppingBasket items;
+  
+
+  @Before
+  public void before() {
+    items = new ShoppingBasket();
+  }
+
+}
